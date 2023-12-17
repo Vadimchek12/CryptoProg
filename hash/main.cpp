c131a02c1d80ad098cdf97bf23aaf0e6fae3127b
VadimShcheglov <145258704+Vadimchek12@users.noreply.github.com>
2023-12-17 18:30:02 +0300

    Create main.cpp

--- hash/main.cpp
+++ hash/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>  
+#include <fstream>  
+#include <cryptopp/sha.h>  
+#include <cryptopp/hex.h>  
+#include <cryptopp/filters.h>  
+#include <cryptopp/files.h>  
+  
+using namespace CryptoPP;  
+  
 
+int main()
+{
+    CryptoPP::SHA256 hash;
+    std::string message = "Hello";
+
+    byte digest[CryptoPP::SHA256::DIGESTSIZE];
+    hash.CalculateDigest(digest, (byte*)message.c_str(), message.length());
+
+    std::cout << "Hash: ";
+    for (int i = 0; i < CryptoPP::SHA256::DIGESTSIZE; i++) {
+        std::cout << std::hex << (int)digest[i];
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
